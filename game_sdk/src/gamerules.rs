426c7bce9b5b26271a9351ff599f5062a6df5fd1
imkgerC <imrund08@gmail.com>
2020-04-06 19:37:03 +0200

    implement set movegen

--- game_sdk/src/gamerules.rs
+++ game_sdk/src/gamerules.rs
@@ -34,12 +34,30 @@ pub fn calculate_legal_moves(game_state: &GameState, actionlist: &mut ActionList
         return;
     }
 
+    let next_to_own =
+        bitboard::get_neighbours(game_state.occupied[game_state.color_to_move as usize]);
+    let next_to_other =
+        bitboard::get_neighbours(game_state.occupied[game_state.color_to_move.swap() as usize]);
+    let mut valid_set_destinations = next_to_own & !(next_to_other | game_state.obstacles);
+
     if game_state.must_player_place_bee() {
         // only bee SetMoves
+        while valid_set_destinations > 0 {
+            let to = valid_set_destinations.trailing_zeros();
+            valid_set_destinations ^= 1 << to;
+            actionlist.push(Action::SetMove(PieceType::BEE, to as u8));
+        }
         return;
     }
 
     // generate SetMoves
+    while valid_set_destinations > 0 {
+        let to = valid_set_destinations.trailing_zeros();
+        valid_set_destinations ^= 1 << to;
+        for piece_type in &crate::piece_type::VARIANTS {
+            actionlist.push(Action::SetMove(*piece_type, to as u8));
+        }
+    }
 
     if game_state.has_player_placed_bee() {
         // generate DragMoves
