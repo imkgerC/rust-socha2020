5d8ea1ef58e18806e3eaa1a2f902003f5229f4e5
imkgerC <imrund08@gmail.com>
2020-04-06 16:11:18 +0200

    structure movegen

--- game_sdk/src/gamerules.rs
+++ game_sdk/src/gamerules.rs
@@ -1,4 +1,25 @@
 use crate::actionlist::ActionList;
 use crate::gamestate::GameState;
 
-pub fn caculate_legal_moves(game_state: GameState, actionlist: &mut ActionList) {}
+pub fn caculate_legal_moves(game_state: GameState, actionlist: &mut ActionList) {
+    if game_state.ply == 0 {
+        // only SetMoves for every piece_type on every field
+        return;
+    }
+
+    if game_state.ply == 1 {
+        // only SetMoves next to only set enemy piece
+        return;
+    }
+
+    if game_state.must_player_set_bee() {
+        // only bee SetMoves
+        return;
+    }
+
+    // generate SetMoves
+
+    if game_state.has_player_placed_bee() {
+        // generate DragMoves
+    }
+}
