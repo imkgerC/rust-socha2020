7e377f679e50ca73ee45d6f4dce727b9962829d9
imkgerC <imrund08@gmail.com>
2020-04-06 15:22:11 +0200

    fix valid fields

--- helper_scripts/bitboard_constants.py
+++ helper_scripts/bitboard_constants.py
@@ -5,10 +5,10 @@ def bit(x, z):
 def in_bounds(x, z):
     distance_to_5 = abs(z - 5)
     if z > 5:
-        if x > distance_to_5:
+        if x >= distance_to_5:
             return True
     elif z < 5:
-        if x < (10 - distance_to_5):
+        if x < (11 - distance_to_5):
             return True
     else:
         return True
@@ -20,12 +20,17 @@ def valid_fields():
         for z in range(11):
             if (in_bounds(x, z)):
                 valid |= bit(x, z)
-    print("Valid fields")
-    print(valid)
-    print(bin(valid))
+    return valid
+
+def print_val(label, val):
+    print(label+":")
+    print("\t"+f"{val}")
+    print("\t"+bin(val))
+
+
 
 def main():
-    valid_fields()
+    print_val("valid_fields", valid_fields())
 
 if __name__ == "__main__":
     main()
